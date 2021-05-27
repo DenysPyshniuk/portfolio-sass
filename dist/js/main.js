221802ce72e6c1cd714f6320a6a5c6b173089ec1
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-26 20:57:06 -0700

    Create a func to hide nav menu when scrolling down

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,3 +25,15 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("menu-nav").style.top = "0";
+  } else {
+    document.getElementById("menu-nav").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
